f62cec1a171dffaf4568420775221c6199a9a49b
jakcooper22 <jakcooper22@gmail.com>
2019-11-21 21:24:59 -0600

    adding img

--- controllers/httpServer.js
+++ controllers/httpServer.js
@@ -2,7 +2,9 @@ var path = require('path')
 var http = require('http');
 var fs   = require('fs');
 
-var dir = path.join(__dirname, 'public')
+var dir = path.join(__dirname, 'views')
+// var dir = path.join(__dirname, 'public')
+// var dir = path.join(__dirname)
 
 http.createServer(function (req, res) {
 
@@ -18,20 +20,23 @@ http.createServer(function (req, res) {
   console.log(tempPath)
 
   if (req.url ==  "/" ) {
+    console.log("hit /");
+    fs.readFile('../views/index1.3.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.end();
+  }
+  if (req.url == "/projects/index.html") {
+    console.log("hit projects/html")
     fs.readFile('../views/index.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();
     });
-  }
-  if (req.url == "/projects/index.html") {
-    console.log("hit projects/html")
-    fs.readFile('../../index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
+  } 
 
   // var files = fs.createReadStream(tempPath)
   // files.on('open', () => {
