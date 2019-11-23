655792eec172de51fd75157bcf1fe02682a07db0
jakcooper22 <jakcooper22@gmail.com>
2019-11-22 20:26:25 -0600

    grabbed website template from w3

--- baseSite/routes/index.js
+++ baseSite/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendfile(__dirname + 'public/index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
 module.exports = router;
