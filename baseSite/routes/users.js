6dd8df00ddfb17973aaf300e73a9f7598650b15d
jakcooper22 <jakcooper22@gmail.com>
2019-11-24 13:43:19 -0600

    adding calendar

--- baseSite/routes/users.js
+++ baseSite/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendfile(__dirname + '/layout.html');
 });
 
 module.exports = router;
