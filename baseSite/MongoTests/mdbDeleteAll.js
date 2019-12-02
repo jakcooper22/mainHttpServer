9c8a6ac98883b7fc0708ddefff8011d00ca85c2a
jakcooper22 <jakcooper22@gmail.com>
2019-12-01 18:56:53 -0600

    add mongo files

--- baseSite/MongoTests/mdbDeleteAll.js
+++ baseSite/MongoTests/mdbDeleteAll.js
@@ -1,3 +1,5 @@
+//databases: CalendarMast, CalMstInsCnt
+
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
@@ -5,7 +7,7 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
   var myquery = { month: /^D/ };
-  dbo.collection("CalendarMast").deleteMany(myquery, function(err, obj) {
+  dbo.collection("CalMstInsCnt").deleteMany(myquery, function(err, obj) {
     if (err) throw err;
     console.log(obj.result.n + " document(s) deleted");
     db.close();
--- baseSite/MongoTests/mdbfindAll.js
+++ baseSite/MongoTests/mdbfindAll.js
@@ -1,3 +1,5 @@
+//databases: CalendarMast, CalMstInsCnt
+
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
@@ -5,7 +7,7 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
   var myobj = { name: "Whatevs Company Inc", address: "Your Mom Highway 37" };
-  dbo.collection("CalendarMast").find().toArray(function(err, res) {
+  dbo.collection("CalMstInsCnt").find().toArray(function(err, res) {
     if (err) throw err;
     console.log(res);
     db.close();
